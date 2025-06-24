d9be0fefc49b574800922341347e63992f43f668
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2025-06-24 18:21:53 +0530

    Fix: Folder Strcuere

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import logsReducer from "./features/logsSlice";
-
-export const store = configureStore({
-  reducer: {
-    logs: logsReducer,
-  },
-});
