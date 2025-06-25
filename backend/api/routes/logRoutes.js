90fdd4e564817bbad73b04cc2de2e4bbbfc62128
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2025-06-25 09:10:06 +0530

    feat: add validation to log creation route using validateCreateLog middleware

--- backend/api/routes/logRoutes.js
+++ backend/api/routes/logRoutes.js
@@ -3,9 +3,10 @@ const express = require("express");
 const router = express.Router();
 
 const { getLogs, createLog } = require("../controllers/logController.js");
+const { validateCreateLog } = require("../validators/logValidator.js");
+const validate = require("../middleware/validationMiddleware.js");
 
 router.get("/", getLogs);
-
-router.post("/", createLog);
+router.post("/", validateCreateLog, validate, createLog);
 
 module.exports = router;
