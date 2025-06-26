1218bd12fc0bed9d89006d88343092a663edf0c7
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2025-06-26 16:30:38 +0530

    feat: add todosReducer to store for managing todo state

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logsReducer from "./features/logsSlice";
+import todosReducer from "./features/todosSlice";
 
 export const store = configureStore({
   reducer: {
     logs: logsReducer,
+    todos: todosReducer,
   },
 });
