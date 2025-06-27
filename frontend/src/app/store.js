b5957b927d22b941fcfa628ba32673cfa6471184
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2025-06-27 13:50:33 +0530

    feat: integrate GitHub slice and service into Redux store for managing GitHub contributions

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logsReducer from "./features/logsSlice";
 import todosReducer from "./features/todosSlice";
+import githubReducer from "./features/githubSlice";
 
 export const store = configureStore({
   reducer: {
     logs: logsReducer,
     todos: todosReducer,
+    github: githubReducer,
   },
 });
