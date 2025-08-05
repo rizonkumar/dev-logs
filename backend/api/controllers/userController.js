7640bc70bb130288d1bce716797ee91449feb5ee
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2025-08-05 07:46:31 +0530

    refactor: migrate user-related logic to userService for improved separation of concerns and maintainability

--- backend/api/controllers/userController.js
+++ backend/api/controllers/userController.js
@@ -1,5 +1,5 @@
 const asyncHandler = require("../middleware/asyncHandler");
-const User = require("../models/userModel");
+const userService = require("../services/userService");
 const generateToken = require("../utils/generateToken");
 
 // @desc    Register a new user
@@ -8,18 +8,7 @@ const generateToken = require("../utils/generateToken");
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
-  const userExists = await User.findOne({ email });
-
-  if (userExists) {
-    res.status(400);
-    throw new Error("User already exists");
-  }
-
-  const user = await User.create({
-    name,
-    email,
-    password,
-  });
+  const user = await userService.createUser({ name, email, password });
 
   if (user) {
     res.status(201).json({
@@ -41,39 +30,30 @@ const registerUser = asyncHandler(async (req, res) => {
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
-  const user = await User.findOne({ email }).select("+password");
+  const user = await userService.authenticateUser(email, password);
 
-  if (user && (await user.matchPassword(password))) {
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      profileImage: user.profileImage,
-      token: generateToken(user._id),
-    });
-  } else {
-    res.status(401);
-    throw new Error("Invalid email or password");
-  }
+  res.json({
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    profileImage: user.profileImage,
+    token: generateToken(user._id),
+  });
 });
 
 // @desc    Get user profile
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user.id);
+  // req.user will be set by our authentication middleware later
+  const user = await userService.getUserById(req.user.id);
 
-  if (user) {
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      profileImage: user.profileImage,
-    });
-  } else {
-    res.status(404);
-    throw new Error("User not found");
-  }
+  res.json({
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    profileImage: user.profileImage,
+  });
 });
 
 module.exports = {
